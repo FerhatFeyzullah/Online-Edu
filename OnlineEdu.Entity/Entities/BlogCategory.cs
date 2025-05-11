b3e65422621dcb57d5ac03075c83977a88c58ce6
FerhatFeyzullah <ferhatfeyzullah@hotmail.com>
2025-05-11 09:48:46 +0300

    first commit and creation of entities

---
+++ OnlineEdu.Entity/Entities/BlogCategory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineEdu.Entity.Entities
+{
+    public class BlogCategory
+    {
+        public int BlogCategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+        public List<Blog> Blogs { get; set; }
+
+
+    }
+}
